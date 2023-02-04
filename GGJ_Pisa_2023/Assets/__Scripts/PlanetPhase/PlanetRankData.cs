cc14a0e251f5e46aae1031f4a3fdf01ec02f2f87
Diciamo michelangelo <mikenigro99@gmail.com>
2023-02-04 13:26:05 +0100

    added ponds spawn mechanic

---
+++ GGJ_Pisa_2023/Assets/__Scripts/PlanetPhase/PlanetRankData.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "_Preset", menuName = "PresetDatas")]
+public class PlanetRankData : SerializedScriptableObject
+{
+   
+
+
+ 
+}
